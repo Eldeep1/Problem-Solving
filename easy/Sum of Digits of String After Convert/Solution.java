526e298068fb7628a9b3e1b2ceb4e5259c01a2c6
unknown <alieldeep111@gmail.com>
2024-09-03 07:40:53 +0300

    solving todays problem

---
+++ easy/Sum of Digits of String After Convert/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getLucky(String s, int k) {
+
+        String convertedString = new String();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            convertedString += (int) s.charAt(i) - 96;
+        }
+        for (int i = 0; i < k; i++) {
+            sum = 0;
+
+            for (int j = 0; j < convertedString.length(); j++) {
+                sum += ((int) convertedString.charAt(j) - 48);
+
+            }
+            convertedString = "" + sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.getLucky("leetcode", 2));
+    }
+}

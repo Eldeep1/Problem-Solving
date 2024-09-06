19a882453b53da4ca8cae55833bd0b43ad1b4a29
unknown <alieldeep111@gmail.com>
2024-09-06 04:15:34 +0300

    it should be easy, not medium ...

--- medium/Delete Nodes From Linked List Present in Array/Solution.java
+++ medium/Delete Nodes From Linked List Present in Array/Solution.java
@@ -2,10 +2,35 @@ import java.util.HashSet;
 import data_structures.ListNode;
 
 public class Solution {
-    HashSet<Integer> numHashSet= new HashSet<>();
     public ListNode modifiedList(int[] nums, ListNode head) {
-     
-        
+        HashSet<Integer> numHashSet= new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            numHashSet.add(nums[i]);
+        }
+        while (true) {
+            if (numHashSet.contains(head.val)) {
+                head=head.next;
+            }
+            else if (head.next==null) {
+                return head;
+            }
+            else{
+                break;
+            }
+        }
+        ListNode back=head;
+        ListNode front=head.next;
+        while (front!=null) {
+            if (numHashSet.contains(front.val)) {
+                front=front.next;
+                back.next=front;
+            }
+            else{
+                back=front;
+                front=front.next;
+            }
+        }
+        return head;
     }
 }
 
